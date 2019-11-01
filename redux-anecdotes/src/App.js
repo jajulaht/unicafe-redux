c434a32be481f24b42b6fae5bdabf26991d2b117
Janne Lahtinen <54285341+jajulaht@users.noreply.github.com>
2019-11-01 09:50:38 +0200

    anekdootit, step 1 done

---
+++ redux-anecdotes/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const App = (props) => {
+  const anecdotes = props.store.getState()
+  const store = props.store
+
+  const vote = (id) => {
+    store.dispatch({
+      type: 'VOTE',
+      data: { id }
+    })
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form>
+        <div><input name='anecdote' /></div>
+        <button type='submit'>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
